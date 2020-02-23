1d89e0f360d462b1b040855875316e06beb0832d
wuanj <wuanj2019@gmail.com>
2020-02-23 22:46:40 +0800

    perf(server/models): change banner model

--- server/libs/db/models/Banner.js
+++ server/libs/db/models/Banner.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose')
 const bannerSchema = new mongoose.Schema({
   categories: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Category' }],
-  banner: {
-    name: { type: String },
-    items: [
-      {
-        title: { type: String },
-        imgUrl: { type: String },
-        targetUrl: { type: String },
-      }
-    ]
-  }
+  name: { type: String },
+  items: [
+    {
+      title: { type: String },
+      imgUrl: { type: String },
+      targetUrl: { type: String },
+    }
+  ]
 }, {
   timestamps: true,
 })
